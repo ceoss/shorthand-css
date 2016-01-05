6908bac4c201cf514252486bdce2446709cbc8ce
Evelyn Hathaway <git@evelynhathaway.com>
2016-01-04 21:29:36 -0800

    Mobile Styles

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@ document.addEventListener("scroll", function(){appbar();});
 // document.addEventListener("DOMContentLoaded", function() {appbar();});
 function appbar() {
 	var appbar = document.getElementsByClassName("appbar")[0];
+	var tabs = document.getElementsByClassName("tabs")[0];
 	var waterfall = document.getElementsByClassName("waterfall")[0];
 	var tester = document.getElementById("tester");
-	if (document.body.scrollTop === 0 && document.documentElement.scrollTop === 0) {
-		appbar.classList.remove("shadow");
-		waterfall.classList.add("falling");
-		tester.classList.add("white");
+	if (window.matchMedia("(max-width: 600px) and (orientation: portrait), (max-width: 960px) and (orientation: landscape)").matches) {
+		if (document.body.scrollTop < 48 && document.documentElement.scrollTop < 48) {
+			document.body.classList.remove("scrolling");
+		} else {
+			document.body.classList.add("scrolling");
+		}
 	} else {
-		appbar.classList.add("shadow");
-		waterfall.classList.remove("falling");
-		tester.classList.remove("white");
+		if (document.body.scrollTop === 0 && document.documentElement.scrollTop === 0) {
+			document.body.classList.remove("scrolling");
+		} else {
+			document.body.classList.add("scrolling");
+		}
 	}
 }
