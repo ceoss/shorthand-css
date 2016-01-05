ead384a05ca84997cce5bfdda7269b22427daf30
Evelyn Hathaway <git@evelynhathaway.com>
2016-01-04 23:11:51 -0800

    Small Changes, Removed Waterfall from Mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-document.addEventListener("scroll", function(){appbar();});
-// document.addEventListener("DOMContentLoaded", function() {appbar();});
 function appbar() {
-	var appbar = document.getElementsByClassName("appbar")[0];
-	var tabs = document.getElementsByClassName("tabs")[0];
-	var waterfall = document.getElementsByClassName("waterfall")[0];
-	var tester = document.getElementById("tester");
 	if (window.matchMedia("(max-width: 600px) and (orientation: portrait), (max-width: 960px) and (orientation: landscape)").matches) {
 		if (document.body.scrollTop < 48 && document.documentElement.scrollTop < 48) {
 			document.body.classList.remove("scrolling");
@@ -19,3 +13,5 @@ function appbar() {
 		}
 	}
 }
+document.addEventListener("scroll", function(){appbar();});
+appbar();
